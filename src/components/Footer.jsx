0742488c65fa75dd4cf789ec358805c6143679b4
Harshit <73424058+harshitprakash@users.noreply.github.com>
2025-11-06 15:02:32 +0530

    home page changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,30 +8,21 @@ const Footer = () => {
         {/* Newsletter Section */}
         <div className="footerarea__newsletter__wraper">
           <div className="row">
-            <div
-              className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12"
-              data-aos="fade-up"
-            >
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" data-aos="fade-up">
               <div className="footerarea__text">
                 <h3>
-                  Still You Need Our <span>Support</span> ?
+                  Still You Need Our <span>Support</span>?
                 </h3>
                 <p>
-                  Don’t wait make a smart & logical quote here. Its pretty easy.
+                  Don’t wait — reach out to us. It’s quick & easy.
                 </p>
               </div>
             </div>
-            <div
-              className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12"
-              data-aos="fade-up"
-            >
+            <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" data-aos="fade-up">
               <div className="footerarea__newsletter">
                 <div className="footerarea__newsletter__input">
                   <form action="#">
-                    <input
-                      type="text"
-                      placeholder="Enter your email here"
-                    />
+                    <input type="email" placeholder="Enter your email here" />
                     <div className="footerarea__newsletter__button">
                       <button type="submit" className="subscribe__btn">
                         Subscribe Now
@@ -47,20 +38,19 @@ const Footer = () => {
         {/* Main Footer Content */}
         <div className="footerarea__wrapper footerarea__wrapper__2">
           <div className="row">
-            {/* About Us */}
-            <div
-              className="col-xl-4 col-lg-4 col-md-6 col-sm-12"
-              data-aos="fade-up"
-            >
+            {/* About / Contact */}
+            <div className="col-xl-5 col-lg-4 col-md-6 col-sm-12" data-aos="fade-up">
               <div className="footerarea__inner footerarea__about__us">
                 <div className="footerarea__heading">
                   <h3>About us</h3>
                 </div>
                 <div className="footerarea__content">
                   <p>
-                    Corporate clients and leisure travelers has been relying on
-                    Groundlink for dependable safe, and professional chauffeured
-                    car end service in major cities across World.
+                    Max Education, near Jain Garments, Samaypur Badli, Delhi — providing professional courses and expert guidance.
+                  </p>
+                  <p>
+                    <strong>Phone:</strong> +91 98101 23456 <br/>
+                    <strong>Email:</strong> info@maxeducation.in
                   </p>
                 </div>
                 <div className="foter__bottom__text">
@@ -69,111 +59,43 @@ const Footer = () => {
                   </div>
                   <div className="footer__bottom__content">
                     <h6>Opening Hours</h6>
-                    <span>Mon - Sat(8.00 - 6.00)</span>
-                    <span>Sunday - Closed</span>
+                    <span>Mon - Sat: 8:00 AM - 6:00 PM</span>
+                    <span>Sunday: Closed</span>
                   </div>
                 </div>
               </div>
             </div>
 
             {/* Useful Links */}
-            <div
-              className="col-xl-2 col-lg-2 col-md-6 col-sm-6"
-              data-aos="fade-up"
-            >
+            <div className="col-xl-3 col-lg-2 col-md-6 col-sm-6" data-aos="fade-up">
               <div className="footerarea__inner">
                 <div className="footerarea__heading">
                   <h3>Useful Links</h3>
                 </div>
                 <div className="footerarea__list">
                   <ul>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Teachers</a></li>
-                    <li><a href="#">Partner</a></li>
-                    <li><a href="#">Room-Details</a></li>
-                    <li><a href="#">Gallery</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About Us</a></li>
+                    <li><a href="/courses">Courses</a></li>
+                    <li><a href="/contact">Contact Us</a></li>
                   </ul>
                 </div>
               </div>
             </div>
 
             {/* Courses */}
-            <div
-              className="col-xl-3 col-lg-3 col-md-6 col-sm-6"
-              data-aos="fade-up"
-            >
+            <div className="col-xl-4 col-lg-3 col-md-6 col-sm-6" data-aos="fade-up">
               <div className="footerarea__inner footerarea__padding__left">
                 <div className="footerarea__heading">
-                  <h3>Course</h3>
+                  <h3>Courses</h3>
                 </div>
                 <div className="footerarea__list">
                   <ul>
-                    <li><a href="#">Ui Ux Design</a></li>
-                    <li><a href="#">Web Development</a></li>
-                    <li><a href="#">Business Strategy</a></li>
-                    <li><a href="#">Software Development</a></li>
-                    <li><a href="#">Business English</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            {/* Recent Posts */}
-            <div
-              className="col-xl-3 col-lg-3 col-md-6 col-sm-12"
-              data-aos="fade-up"
-            >
-              <div className="footerarea__right__wraper footerarea__inner">
-                <div className="footerarea__heading">
-                  <h3>Recent Post</h3>
-                </div>
-                <div className="footerarea__right__list">
-                  <ul>
-                    <li>
-                      <a href="#">
-                        <div className="footerarea__right__img">
-                          <img
-                            loading="lazy"
-                            src="img/footer/footer__1.png"
-                            alt="footerphoto"
-                          />
-                        </div>
-                        <div className="footerarea__right__content">
-                          <span>02 Apr 2024 </span>
-                          <h6>Best Your Business</h6>
-                        </div>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <div className="footerarea__right__img">
-                          <img
-                            loading="lazy"
-                            src="img/footer/footer__2.png"
-                            alt="footerphoto"
-                          />
-                        </div>
-                        <div className="footerarea__right__content">
-                          <span>02 Apr 2024 </span>
-                          <h6>Keep Your Business</h6>
-                        </div>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <div className="footerarea__right__img">
-                          <img
-                            loading="lazy"
-                            src="img/footer/footer__3.png"
-                            alt="footerphoto"
-                          />
-                        </div>
-                        <div className="footerarea__right__content">
-                          <span>02 Apr 2024 </span>
-                          <h6>Nice Your Business</h6>
-                        </div>
-                      </a>
-                    </li>
+                    <li><a href="/courses/ui-ux">UI/UX Design</a></li>
+                    <li><a href="/courses/web-dev">Web Development</a></li>
+                    <li><a href="/courses/business">Business Strategy</a></li>
+                    <li><a href="/courses/software-dev">Software Development</a></li>
+                    <li><a href="/courses/english">Business English</a></li>
                   </ul>
                 </div>
               </div>
@@ -183,54 +105,28 @@ const Footer = () => {
 
         {/* Copyright */}
         <div className="footerarea__copyright__wrapper footerarea__copyright__wrapper__2">
-          <div className="row">
+          <div className="row align-items-center">
             <div className="col-xl-3 col-lg-3">
               <div className="copyright__logo">
                 <a href="/">
-                  <img
-                    loading="lazy"
-                    src="img/logo/logo_2.png"
-                    alt="logo"
-                  />
+                  <img loading="lazy" src="img/logo/logo_2.png" alt="logo" />
                 </a>
               </div>
             </div>
             <div className="col-xl-6 col-lg-6">
-              <div className="footerarea__copyright__content footerarea__copyright__content__2">
+              <div className="footerarea__copyright__content footerarea__copyright__content__2 text-center">
                 <p>
-                  Copyright © <span>2024</span> by edurock. All Rights
-                  Reserved.
+                  Copyright © <span>2024</span> by Max Education. All Rights Reserved.
                 </p>
               </div>
             </div>
-            <div className="col-xl-3 col-lg-3">
+            <div className="col-xl-3 col-lg-3 text-end">
               <div className="footerarea__icon footerarea__icon__2">
                 <ul>
-                  <li>
-                    <a href="//facebook.com">
-                      <i className="icofont-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="//twitter.com">
-                      <i className="icofont-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="//vimeo.com">
-                      <i className="icofont-vimeo"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="//linkedin.com">
-                      <i className="icofont-linkedin"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="//skype.com">
-                      <i className="icofont-skype"></i>
-                    </a>
-                  </li>
+                  <li><a href="//facebook.com/maxeducation" target="_blank"><i className="icofont-facebook"></i></a></li>
+                  <li><a href="//twitter.com/maxeducation" target="_blank"><i className="icofont-twitter"></i></a></li>
+                  <li><a href="//linkedin.com/company/maxeducation" target="_blank"><i className="icofont-linkedin"></i></a></li>
+                  <li><a href="//instagram.com/maxeducation" target="_blank"><i className="icofont-instagram"></i></a></li>
                 </ul>
               </div>
             </div>
